1e77141eb151a395dba8afc9f53ff009677eb38d
ClaymorePlay <112704976+ClaymorePlay@users.noreply.github.com>
2023-04-21 00:47:46 +0300

    Добавление wsclient

--- PharmacySystem.Client/Program.cs
+++ PharmacySystem.Client/Program.cs
@@ -1,17 +1,41 @@
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
+
 namespace PharmacySystem.Client
 {
     internal static class Program
     {
+        public static WsConnection _wsConnection { get; set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            //_wsConnection = new WsConnection();
+            //await _wsConnection.OpenConnectionAsync(CancellationToken.None);
+
+
+            var client = new WsConnection();
+
+            client.OnMessage += Client_OnMessage;
+            await client.Start();
+
+            
+            WsConnection._weClient.SendAsync("", );
+
             Application.Run(new Form1());
+        }
+
+        private static void Client_OnMessage(object sender, string message)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ PharmacySystem.Client/WsConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmacySystem.Client
+{
+    public class WsConnection
+    {
+        public static ClientWebSocket _weClient { get; set; }
+
+        // Declare the delegate (if using non-generic pattern).
+        public delegate void SocketEventHandler(object sender, string message);
+
+        public event SocketEventHandler OnMessage;
+
+        public async Task Start()
+        {
+            using var ws = new ClientWebSocket();
+
+            await ws.ConnectAsync(new Uri($"wss://localhost:44356/ws?token={Guid.NewGuid()}"), CancellationToken.None);
+            byte[] buf = new byte[4056];
+
+            while (ws.State == WebSocketState.Open)
+            {
+                var result = await ws.ReceiveAsync(buf, CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                }
+                else
+                {
+                    OnMessage.Invoke(this, Encoding.ASCII.GetString(buf, 0, result.Count));
+                }
+            }
+        }
+    }
+}
